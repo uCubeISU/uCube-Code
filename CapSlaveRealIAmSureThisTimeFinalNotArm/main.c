09a37c8e05e9ae42b61d03b50a0d21fa1e0da50e
Jeramie <jeramie.vens@gmail.com>
2015-05-03 05:34:31 -0500

    stuff

--- CapSlaveRealIAmSureThisTimeFinalNotArm/main.c
+++ CapSlaveRealIAmSureThisTimeFinalNotArm/main.c
@@ -1,4 +1,8 @@
-#include <msp430.h> 
+#include <msp430.h>
+#include "CapSens/CTS_Layer.h"
+#include <stdint.h>
+
+#define TRUNK_VALUE(x, min, max) (x = x < min ? min: x > max ? max : x)
 
 /*
  * main.c
@@ -18,9 +22,6 @@ int main(void) {
 
 	UCA0CTL0 |= UCCKPL + UCMSB + UCSYNC + 4;  // 4-pin, 8-bit SPI slave
 	UCA0CTL1 |= UCSSEL_2;                     // SMCLK
-	//UCA0BR0 |= 0x02;                          // /2
-	//UCA0BR1 = 0;                              //
-	//UCB0MCTL = 0;                             // No modulation
 	UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
 	__enable_interrupt();
 	IFG2 = 0;
@@ -28,10 +29,62 @@ int main(void) {
 	UCA0TXBUF = 0xCC;
 
 
+
+	uint16_t cap_raw_value[16];
+	uint8_t cap_value[5][5];
+	int i, j;
+	for(i=0;i<5;i++)for(j=0;j<5;j++)
+		cap_value[i][j] = 0;
+
+
+
+
+
+
+
+
+
+
 	while (1)
 	{
-	//	while(IFG2 & UCA0TXIFG == 0);
-		//UCA0TXBUF = 0xCC;
+		TI_CAPT_Raw(&sensor_1,cap_raw_value);
+
+		/*Puts raw capacitive touch value into correct place in the 5x5 panel*/
+		cap_value[0][0] = 255-((TRUNK_VALUE(cap_raw_value[0], 6000, 14191) - 6000) >> 5);
+		cap_value[0][1] = 255-((TRUNK_VALUE(cap_raw_value[1], 6000, 14191) - 6000) >> 5);
+		cap_value[0][2] = 255-((TRUNK_VALUE(cap_raw_value[2], 6000, 14191) - 6000) >> 5);
+		cap_value[0][3] = 255-((TRUNK_VALUE(cap_raw_value[3], 6000, 14191) - 6000) >> 5);
+		cap_value[0][4] = 255-((TRUNK_VALUE(cap_raw_value[4], 6000, 14191) - 6000) >> 5);
+		cap_value[1][0] = 255-((TRUNK_VALUE(cap_raw_value[5], 6000, 14191) - 6000) >> 5);
+		cap_value[1][1] = 255-((TRUNK_VALUE(cap_raw_value[6], 6000, 14191) - 6000) >> 5);
+		cap_value[1][2] = 255-((TRUNK_VALUE(cap_raw_value[7], 6000, 14191) - 6000) >> 5);
+		cap_value[1][3] = 255-((TRUNK_VALUE(cap_raw_value[8], 6000, 14191) - 6000) >> 5);
+		cap_value[1][4] = 255-((TRUNK_VALUE(cap_raw_value[9], 6000, 14191) - 6000) >> 5);
+		cap_value[2][1] = 255-((TRUNK_VALUE(cap_raw_value[10], 6000, 14191) - 6000) >> 5);
+		cap_value[2][2] = 255-((TRUNK_VALUE(cap_raw_value[11], 6000, 14191) - 6000) >> 5);
+		cap_value[2][3] = 255-((TRUNK_VALUE(cap_raw_value[12], 6000, 14191) - 6000) >> 5);
+		cap_value[2][4] = 255-((TRUNK_VALUE(cap_raw_value[13], 6000, 14191) - 6000) >> 5);
+		cap_value[3][3] = 255-((TRUNK_VALUE(cap_raw_value[14], 6000, 14191) - 6000) >> 5);
+		cap_value[3][4] = 255-((TRUNK_VALUE(cap_raw_value[15], 6000, 14191) - 6000) >> 5);
+
+
+		int x, y;
+		for(i=0;i<5;i++){
+			for(j=0;j<4;j++){
+				if(cap_value[i][j] != 0 && cap_value[i][j+1]){
+					x = ((j<<7) + 64 + (cap_value[i][j+1]>>2) - (cap_value[i][j]>>2));
+				}
+			}
+		}
+
+		for(i=0;i<4;i++){
+			for(j=0;j<5;j++){
+				if(cap_value[i][j] != 0 && cap_value[i+1][j]){
+					y = ((i<<7) + 64 + (cap_value[i+1][j]>>2) - (cap_value[i][j]>>2));
+				}
+			}
+		}
+
 		P2OUT ^= 0xC0;								// Toggle LED on and off
 		_delay_cycles(1000000);
 	}
